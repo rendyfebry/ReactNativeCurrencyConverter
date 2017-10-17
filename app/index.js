a1bcaa59b6a081bb9bff45707d12cc9dd5d40799
Rendy Febry <rendyfebry@gmail.com>
2017-10-17 16:31:36 +0700

    Setup ESLint

--- app/index.js
+++ app/index.js
@@ -1,19 +1,6 @@
 import React from 'react'
-import {
-	StyleSheet,
-	Text,
-	View,
-} from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default class App extends React.Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text>Index JS. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-			</View>
-		);
-	}
-}
 
 const styles = StyleSheet.create({
 	container: {
@@ -22,4 +9,13 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-});
+})
+
+// eslint-disable-next-line react/prefer-stateless-function
+const App = () => (
+	<View style={styles.container}>
+		<Text>Index JS. Lorem ipsum dolor sit amet. Test.</Text>
+	</View>
+)
+
+export default App
