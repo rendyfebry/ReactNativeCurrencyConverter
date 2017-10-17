548553f56f30196252eb8334c66947a11ced2d64
Rendy Febry <rendyfebry@gmail.com>
2017-10-17 16:01:24 +0700

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native'
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Index JS. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
